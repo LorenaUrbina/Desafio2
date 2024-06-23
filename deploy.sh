122e3e1a44e5766390db4c17130dd4cd4a417f19
Lorena <lorenakv17@gmail.com>
2024-06-23 03:33:24 -0400

    dawd

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 STACK_NAME=hello-world-stack
 REGION=us-west-2
 TEMPLATE_FILE=template.yaml
-S3_BUCKET=YOUR_S3_BUCKET  # Reemplaza con el nombre de tu bucket S3
 
 # Eliminar el stack si está en estado ROLLBACK_COMPLETE
 if aws cloudformation describe-stacks --stack-name $STACK_NAME --region $REGION | grep -q "ROLLBACK_COMPLETE"; then
@@ -12,5 +11,5 @@ if aws cloudformation describe-stacks --stack-name $STACK_NAME --region $REGION 
     aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME --region $REGION
 fi
 
-aws cloudformation package --template-file $TEMPLATE_FILE --s3-bucket $S3_BUCKET --output-template-file packaged.yaml --region $REGION
-aws cloudformation deploy --template-file packaged.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_IAM --region $REGION
+# Desplegar la plantilla
+aws cloudformation deploy --template-file $TEMPLATE_FILE --stack-name $STACK_NAME --capabilities CAPABILITY_IAM --region $REGION
