418eb78ff0a0335fde916ddd36ead23396aa544f
Lorena <lorenakv17@gmail.com>
2024-06-23 04:27:09 -0400

    dwdwd

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,15 +2,14 @@ pipeline {
     agent any
 
     environment {
-        AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
-        AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
-        AWS_DEFAULT_REGION = 'us-west-2'
+        AWS_ACCESS_KEY_ID = credentials('aws-access-key-id')
+        AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
     }
 
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/LorenaUrbina/Desafio2.git', branch: 'main'
+                git 'https://github.com/LorenaUrbina/Desafio2.git'
             }
         }
         stage('Deploy') {
@@ -22,9 +21,8 @@ pipeline {
         stage('Get API URL') {
             steps {
                 script {
-                    def apiUrl = sh(script: "aws cloudformation describe-stacks --stack-name hello-world-stack --query 'Stacks[0].Outputs[?OutputKey==`HelloWorldApiUrl`].OutputValue' --output text", returnStdout: true).trim()
+                    def apiUrl = sh(script: "aws cloudformation describe-stacks --stack-name hello-world-stack --query Stacks[0].Outputs[?OutputKey=='HelloWorldApiUrl'].OutputValue --output text", returnStdout: true).trim()
                     echo "API Endpoint is: ${apiUrl}"
-                    sh "curl -v ${apiUrl}" // Esto imprimirá detalles de la conexión curl
                 }
             }
         }
--- deploy.sh
+++ deploy.sh
@@ -3,30 +3,17 @@
 STACK_NAME=hello-world-stack
 REGION=us-west-2
 TEMPLATE_FILE=template.yaml
-S3_BUCKET=Lrbucket
+S3_BUCKET=lr-bucket-s3
 
-# Crear un bucket de S3 si no existe
-if ! aws s3api head-bucket --bucket "$S3_BUCKET" 2>/dev/null; then
-    aws s3api create-bucket --bucket "$S3_BUCKET" --region $REGION --create-bucket-configuration LocationConstraint=$REGION
+# Crear el bucket S3 si no existe
+if ! aws s3api head-bucket --bucket $S3_BUCKET 2>/dev/null; then
+    aws s3api create-bucket --bucket $S3_BUCKET --region $REGION --create-bucket-configuration LocationConstraint=$REGION
     echo "S3 bucket created: $S3_BUCKET"
 fi
 
-# Eliminar el stack si está en estado ROLLBACK_COMPLETE
-if aws cloudformation describe-stacks --stack-name $STACK_NAME --region $REGION | grep -q "ROLLBACK_COMPLETE"; then
-    aws cloudformation delete-stack --stack-name $STACK_NAME --region $REGION
-    echo "Waiting for stack deletion to complete..."
-    aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME --region $REGION
-fi
-
-# Verificar si el archivo lambda_function.zip existe
-if [ ! -f lambda_function.zip ]; then
-    echo "The file lambda_function.zip does not exist."
-    exit 1
-fi
-
-# Subir el código a S3
+# Subir el archivo zip a S3
 aws s3 cp lambda_function.zip s3://$S3_BUCKET/
 
 # Empaquetar y desplegar la plantilla
-aws cloudformation package --template-file $TEMPLATE_FILE --s3-bucket $S3_BUCKET --output-template-file packaged.yaml --region $REGION
+aws cloudformation package --template-file $TEMPLATE_FILE --s3-bucket $S3_BUCKET --output-template-file packaged.yaml
 aws cloudformation deploy --template-file packaged.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_IAM --region $REGION
